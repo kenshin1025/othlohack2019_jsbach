7d4cecdbe80e61b23a941d05fcab632e2093bae7
koki_akazaki <akzkouki@gmail.com>
2019-11-02 22:51:04 +0900

    BeatPieceを追加

---
+++ src/components/BeatPiece.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="beatPiece">
+    <template v-for="i in this.notes">
+        {{i}}
+      <!-- <ChordNode class="flex_item" :cursor_bar="i-1"></ChordNode> -->
+    </template>
+  </div>
+</template>
+
+<script>
+import ChordNode from "../components/ChordNode";
+
+export default {
+  name: "Chords",
+  components: {
+    ChordNode
+  },
+  props:["notes","beatType"]
+};
+</script>
+
+<style scoped>
+.chords {
+  display: flex;
+}
+.flex_item {
+  flex-grow: 1;
+}
+</style>
