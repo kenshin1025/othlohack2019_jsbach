3f1edd291b1c6832348f3ac4119f22d505d6384e
x18067xx <x18067xx@aitech.ac.jp>
2019-11-02 14:34:24 +0900

    いらないハローワールドコンポーネント消した

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <!--<img alt="Vue logo" src="../assets/logo.png">-->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <CodeNode class="flex_item"></CodeNode>
     <CodeNode class="flex_item"></CodeNode>
     <CodeNode class="flex_item"></CodeNode>
@@ -10,15 +8,12 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import CodeNode from '../components/CodeNode'
 
 export default {
   name: 'home',
   components: {
       CodeNode,
-    HelloWorld
   }
 }
 </script>
