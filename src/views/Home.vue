70e82464f8c0811a3fe7a597f08e9be1a8ceef8b
x18067xx <x18067xx@aitech.ac.jp>
2019-11-02 14:24:21 +0900

    １つ１つのコードの入れ物完成した

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <CodeNode class="flex_item"></CodeNode>
+    <CodeNode class="flex_item"></CodeNode>
+    <CodeNode class="flex_item"></CodeNode>
+    <CodeNode class="flex_item"></CodeNode>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import CodeNode from '../components/CodeNode'
 
 export default {
   name: 'home',
   components: {
+      CodeNode,
     HelloWorld
   }
 }
 </script>
+
+<style scoped>
+  .home{
+    display: flex;
+  }
+
+  .flex_item{
+    flex-grow: 1;
+  }
+</style>
