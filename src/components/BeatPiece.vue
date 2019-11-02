edb1e34f660e6c581745ccaeebfb805ef7601855
x18067xx <x18067xx@aitech.ac.jp>
2019-11-02 22:58:23 +0900

    二人の作業を合わせた

--- src/components/BeatPiece.vue
+++ src/components/BeatPiece.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="beatPiece">
     <template v-for="i in this.notes">
-        {{i}}
-      <!-- <ChordNode class="flex_item" :cursor_bar="i-1"></ChordNode> -->
+       <BeatNode class="flex_item" :sound="i"></BeatNode>
+       <!--<ChordNode class="flex_item" :cursor_bar="i-1"></ChordNode>-->
     </template>
   </div>
 </template>
 
 <script>
-import ChordNode from "../components/ChordNode";
+import BeatNode from "../components/BeatsNode";
 
 export default {
-  name: "Chords",
+  name: "BeatPiece",
   components: {
-    ChordNode
+      BeatNode
   },
   props:["notes","beatType"]
 };
 </script>
 
 <style scoped>
-.chords {
+.beatPiece {
   display: flex;
 }
 .flex_item {
